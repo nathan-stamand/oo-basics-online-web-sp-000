b58e1c859b5333b9f9ca153588717624a5b1ade2
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-24 22:44:36 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book 
-  attr_accessor :title, :author(name), :page_count(num), :genre(genre)
+  attr_accessor :title, :author, :page_count, :genre
   
   def initialize(title)
     @title = title
